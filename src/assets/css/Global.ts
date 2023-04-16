fb04a712ba949a828871588cb4a7a346ca54e87d
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-16 12:31:49 -0300

    style: global css

---
+++ src/assets/css/Global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalCSS = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
+
+export default GlobalCSS
